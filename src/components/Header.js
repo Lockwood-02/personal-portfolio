974ce9ad419b31cb867bc62a649d47ac86dbbe86
Juno <isaaclockwood618@gmail.com>
2023-03-19 03:16:19 -0500

    Added dark mode for mobile
    Forgot the button disappeared on mobile should be fixed now

--- src/components/Header.js
+++ src/components/Header.js
@@ -82,6 +82,11 @@ function Header() {
 
             {/* Mobile Menu */}
             <div className="md:hidden flex justify-center items-center">
+            <button className='md:hidden flex items-center justify-center border rounded h-10 w-10 border-[#2c2c2c] dark:border-gray-400 mr-4' onClick={toggleTheme}>
+                    {/* <img className='flex h-7 w-7' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAYlJREFUWEftmDFuwkAQRe0rRIECWpSWs+QE1ClzDgpKak6QU0TQ0kZROigSixMggbD9LXms4a/HcZhFoVnh3WX/vBnPzpAmHT+jh+R07Sf2hyTtckSnzZeD3QqUwnafiwLUz2s+ZB+jfJzO9jWAbYmaCboXKONKE5y9z2tLQTSUpJlgdAI1wSw2GclfIxidQAhGbFpJ9kYweoEwQOZLGZM3I+hGYFdX907wbgR+bY65Lc/L73xELLoh+C+QFaUsYd+MoCZMppn4Bcp6jlUZzKWh6QXrKMFoBKLqGE+K3sJKMjT2WhP0L3Bd2jQoujSQhKUaUdZEybeWkcN8dZNULvEucLsq+tnHp3o/myhEYWlVMeNB2R9r5MwE3QqUeUsVioUl0SofBhILJdeIwWgFajdGI0bLhegxtH24MTAv6z+5r1EP4m2Gi90JlK7Gdya4LTGWV9UYjEagJCJvireXoQat9hwxFkqMxqB2qnuBjKhmmLUaojHI/Mf+m7a61OxiJriv+T/ri60GnAG1B7A4DYusaQAAAABJRU5ErkJggg==" alt="Theme" /> */}
+              {theme === 'dark' ? sun : moon}
+            </button>
+            
             <a href='/' className='flex md:hidden justify-center text-xl font-bold dark:text-stone-300'>
                 <span>Isaac Lockwood</span>
             </a>
