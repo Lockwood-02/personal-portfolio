93af11485f6bdfc4d41fe95db5154ee547bc60e4
Juno <nitrorexxo@gmail.com>
2023-11-12 17:09:40 -0600

    Updated Resume Link
    Added a new link for my updated resume on google docs

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,23 +15,23 @@ function About() {
                         </p>
                         <p className="text-xl mt-8">
                             I consider myself a "forever student," eager to both build my knowledge over the vast spectrum
-                            of computer science and staying on top of the most recent developments in the computer science 
+                            of computer science and staying on top of the most recent developments in the computer science
                             industry.
                         </p>
                         <p className="text-xl mt-8">
-                            Right now I am open looking for work. If you would like to get in touch with me you can email me 
+                            Right now I am open looking for work. If you would like to get in touch with me you can email me
                             at <a href="mailto:isaaclockwood618@gmail.com" className="text-amber-500 underline hover:bg-amber-500 hover:text-stone-300">
-                                isaaclockwood618@gmail.com</a> You can also view my resume <a href="https://i.postimg.cc/52MxfVBf/Isaac-Lockwood-Resume-2023-q.png" target="_blank" rel="noreferrer" className="text-amber-500 underline hover:bg-amber-500 hover:text-stone-300">
+                                isaaclockwood618@gmail.com</a> You can also view my resume <a href="https://docs.google.com/document/d/1ocPddHSScaqN3ZLxyrtEIY3cpP6V80Je0LxbB-anp9Q/edit?usp=sharing" target="_blank" rel="noreferrer" className="text-amber-500 underline hover:bg-amber-500 hover:text-stone-300">
                                 here.</a>
                         </p>
                     </div>
                 </header>
                 <div className="flex mt-20 ml-8 m-auto object-bottom hidden md:block">
-                    <img src="https://i.postimg.cc/SNMv2FkR/fotor-2023-3-15-0-4-55.jpg" alt="me" title="MeOnDaPhotoShoot" className="border rounded border-[#2c2c2c] dark:border-gray-400"/>
+                    <img src="https://i.postimg.cc/SNMv2FkR/fotor-2023-3-15-0-4-55.jpg" alt="me" title="MeOnDaPhotoShoot" className="border rounded border-[#2c2c2c] dark:border-gray-400" />
                 </div>
             </div>
             <div className="flex w-full h-auto m-auto mx-auto mt-8 md:hidden justify-center">
-                    <img src="https://i.postimg.cc/SNMv2FkR/fotor-2023-3-15-0-4-55.jpg" alt="me" title="MeOnDaPhotoShoot" className="border rounded border-[#2c2c2c] dark:border-gray-400"/>
+                <img src="https://i.postimg.cc/SNMv2FkR/fotor-2023-3-15-0-4-55.jpg" alt="me" title="MeOnDaPhotoShoot" className="border rounded border-[#2c2c2c] dark:border-gray-400" />
             </div>
         </>
     )
